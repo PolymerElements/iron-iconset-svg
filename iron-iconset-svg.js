b64c5081c44ee892a49e7fd9314fe3f7b68925a5
Russell Bicknell <bicknellr@google.com>
2018-07-16 16:34:42 -0700

    format

--- iron-iconset-svg.js
+++ iron-iconset-svg.js
@@ -1,17 +1,18 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 import '@polymer/polymer/polymer-legacy.js';
 
-import { IronMeta } from '@polymer/iron-meta/iron-meta.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
+import {IronMeta} from '@polymer/iron-meta/iron-meta.js';
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
 /**
  * The `iron-iconset-svg` element allows users to define their own icon sets
  * that contain svg icons. The svg icon elements should be children of the
@@ -82,8 +83,7 @@ Polymer({
   },
 
   created: function() {
-    this._meta =
-        new IronMeta({type: 'iconset', key: null, value: null});
+    this._meta = new IronMeta({type: 'iconset', key: null, value: null});
   },
 
   attached: function() {
